e5f8c79efbd2c985bb8efc8db680832449f5fb0f
AhhhhhChiu <ahchiu9808@gmail.com>
2023-03-01 21:02:46 +0800

    feat: vue.js设计与实现demo - 建立副作用函数与被操作的目标字段之间的联系

--- vue.js设计与实现/demo.js
+++ vue.js设计与实现/demo.js
@@ -1,29 +1,54 @@
-// 注册副作用函数
+/**
+ * 注册副作用函数
+ */
 let activeEffect
 const effect = (fn) => {
   activeEffect = fn
   fn()
 }
 
-// 收集依赖的桶
-const bucket = new Set()
+/**
+ * 收集依赖
+ */
+const bucket = new WeakMap()
+const track = (target, key) => {
+  console.log('track', key)
+  if (!activeEffect) return 
+  let depsMap = bucket.get(target)
+  if (!depsMap) bucket.set(target, (depsMap = new Map()))
+  let deps = depsMap.get(key)
+  if (!deps) depsMap.set(key, (deps = new Set()))
+  deps.add(activeEffect)
+}
 
-// 代理
+/**
+ * 触发依赖执行
+ */
+const trigger = (target, key) => {
+  console.log('trigger', key)
+  const depsMap = bucket.get(target)
+  if (!depsMap) return
+  const deps = depsMap.get(key)
+  deps && deps.forEach((fn) => fn())
+}
+
+/**
+ * 代理
+ */
 const proxy = (data) => new Proxy(data, {
   get(target, key) {
-    console.log('get', key)
-    activeEffect && bucket.add(activeEffect) // 读取属性时收集依赖
+    track(target, key) // 读取属性时收集依赖
     return target[key]
   },
   set(target, key, newVal) {
-    console.log('set', key)
     target[key] = newVal
-    bucket.forEach((fn) => fn()) // 写入属性时触发依赖执行
-    return true
+    trigger(target, key) // 写入属性时触发依赖执行
   }
 })
 
-// 使用
+/**
+ * 使用
+ */
 const obj = proxy({ foo: 1 })
 effect(() => {
   console.log('effect')
